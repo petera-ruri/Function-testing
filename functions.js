bef701bcdef038c894d02c404b0be567df49f1e8
Peteraruri <adjust1993@gmail.com>
2024-02-22 21:28:40 +1300

    converter completed

--- functions.js
+++ functions.js
@@ -1,24 +1,12 @@
-function changeText() {
-    document.getElementsByClassName('title')[0].innerHTML = "Hello World";   
-}
+function tempatureConverter() {
+    let userInput = document.getElementById("input").value;
+    let userType = document.getElementById("type").value;
+    let convertedValue;
+    if (userType == "Celsius") {
+        convertedValue = (userInput + 32) * 9 / 5;
+    } else {
+        convertedValue = (userInput * 9 / 5) - 32;
+    }
+    document.getElementById("result").value = convertedValue;
 
-
-function resetText() {
-    document.getElementsByClassName('title')[0].innerHTML = "Functions";
-    document.getElementById('img').style.display = 'none';
-    document.getElementsByClassName('list')[0].removeChild (document.getElementsByTagName('li')[0]); document.getElementsByTagName('input')[0].value = ""; 
-}
-
-function showImage() {
-    document.getElementById('img').style.display = 'block';
-}
-
-function showList() {
-    // const ul = document.createElement('ul');
-    const li = document.createElement('li');
-    document.getElementsByClassName('list')[0]
-    .appendChild(li);
-    li.innerHTML = document.getElementsByTagName('input')[0]
-    .value; 
-    document.getElementsByTagName('input')[0].value = "";
 }
